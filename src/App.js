dc6530925f2de707127e46d4e1555461c482e3c0
viclafouch <victor.dlf@outlook.fr>
2020-06-24 15:02:49 +0200

    change article dev.to

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>React Modal from scratch</p>
-          <a href="https://dev.to/viclafouch/build-a-complete-modal-component-with-react-hooks-3309">Article on dev.to</a>
+          <a href="https://dev.to/viclafouch/build-a-complete-modal-component-with-react-hooks-4nl0">Article on dev.to</a>
           <a href="https://gist.github.com/viclafouch/6ee36b2cb7d28484d20f05e68b3433f9">Source code here</a>
           <button className="App-button" onClick={() => modal.current.open()}>
             Open me!
